77b352da50b4a56e31bd182f1f0f62436e5d4e3c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-02 13:41:23 +0000

    set up GeoSchema

--- models/ninja.js
+++ models/ninja.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// create geolocation schema
+const GeoSchema = new Schema({
+   type: {
+       type: String,
+       default: "Point"
+   },
+   coordinates: {
+       type: [Number],
+       index: "2dsphere"
+   }
+});
+
 // create ninja Schema & model 
 const NinjaSchema = new Schema({
     name: {
